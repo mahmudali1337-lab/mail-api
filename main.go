4a2d26c73cd256f449df0f248cbc0573683da426
mahmudali1337-lab <mahmudali1337@github.com>
2026-05-03 04:09:19 +0500

    fix: use AUTH LOGIN instead of PLAIN

--- main.go
+++ main.go
@@ -89,6 +89,28 @@ func (a unencryptedAuth) Start(server *smtp.ServerInfo) (string, []byte, error) 
 	return a.Auth.Start(&s)
 }
 
+type loginAuth struct {
+	username, password string
+}
+
+func (a loginAuth) Start(_ *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", nil, nil
+}
+
+func (a loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch strings.ToLower(string(fromServer)) {
+		case "username:":
+			return []byte(a.username), nil
+		case "password:":
+			return []byte(a.password), nil
+		default:
+			return nil, fmt.Errorf("unexpected server challenge: %s", fromServer)
+		}
+	}
+	return nil, nil
+}
+
 func sendMail(relay Relay, req SendRequest) error {
 	addr := fmt.Sprintf("%s:%d", relay.Host, relay.Port)
 
@@ -108,8 +130,8 @@ func sendMail(relay Relay, req SendRequest) error {
 		c.StartTLS(&tls.Config{ServerName: relay.Host, InsecureSkipVerify: true})
 	}
 
-	auth := smtp.PlainAuth("", relay.From, relay.Password, relay.Host)
-	if err := c.Auth(unencryptedAuth{auth}); err != nil {
+	auth := loginAuth{username: relay.From, password: relay.Password}
+	if err := c.Auth(auth); err != nil {
 		return fmt.Errorf("auth: %w", err)
 	}
 
